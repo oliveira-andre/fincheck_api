c1be3a231722568d7931c349d66653fe8ce9b1fa
Andre Oliveira <andreoliveirar2d2@gmail.com>
2025-11-09 12:30:15 -0300

    feat: creating user on db with route, controller and service

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
