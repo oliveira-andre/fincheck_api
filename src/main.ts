9fc8663640c970d6a7ab7e4c03277fa6c99e42cd
Andre Oliveira <andreoliveirar2d2@gmail.com>
2025-11-19 22:43:31 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
