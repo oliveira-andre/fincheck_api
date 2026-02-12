49311f3cb6e043f9e9a59bbf4ad564d68fc45a7c
Andre Oliveira <andreoliveirar2d2@gmail.com>
2026-02-12 11:46:00 -0300

    feat: validate category fields

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,8 +1,22 @@
 import { TransactionType } from "src/modules/transactions/entities/Transaction";
+import { IsString, IsNotEmpty, MaxLength, IsOptional, IsEnum, IsUUID } from "class-validator";
 
 export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
   name: string;
+
+  @IsString()
+  @IsOptional()
   icon: string;
+
+  @IsEnum(TransactionType)
+  @IsNotEmpty()
   type: TransactionType;
+
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   bankAccountId?: string | null | undefined;
 }
